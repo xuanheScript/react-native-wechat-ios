264fb7f8512773e32689fabd9101a4c76a152989
李杰 <lj706517903@163.com>
2017-07-05 09:45:24 +0800

    Update RCTWeChat.h

--- RCTWeChat/RCTWeChat/RCTWeChat.h
+++ RCTWeChat/RCTWeChat/RCTWeChat.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
 #import "WXApi.h"
 
 @interface RCTWeChat : NSObject <RCTBridgeModule, WXApiDelegate>
